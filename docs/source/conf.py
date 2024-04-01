9bdfe6956498acc3d498dfa4ef2ac5b87d994423
Polydot <honema@live.nl>
2024-04-01 10:34:40 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,16 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+## conf.py
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'custom.css',
+]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
